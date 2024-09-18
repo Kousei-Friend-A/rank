6963452b2a72289caa7b24deed3fc93175dbf622
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-18 10:55:06 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -9,18 +9,35 @@ chatdb = mongo.chat
 def increase_count(chat, user):
     user = str(user)
     today = str(date.today())
-    user_db = chatdb.find_one({"chat": chat})
-
-    if not user_db or today not in user_db:
-        user_db = {today: {}}
-    else:
-        user_db = user_db[today]
-
+    
     chatdb.update_one(
         {"chat": chat},
-        {"$inc": {f"{today}.{user}": 1}},
+        {
+            "$setOnInsert": {today: {}},  # Create today's entry if it doesn't exist
+            "$inc": {f"{today}.{user}": 1}  # Increment the user's count for today
+        },
         upsert=True
     )
+
+def get_user_profile(chat, user):
+    user = str(user)
+    today = str(date.today())
+    
+    user_data = chatdb.find_one({"chat": chat})
+
+    if not user_data or today not in user_data:
+        return {
+            "today_count": 0,
+            "total_count": 0
+        }
+
+    today_count = user_data[today].get(user, 0)  # Messages sent today
+    total_count = sum(user_data[today].values())  # Total messages today
+
+    return {
+        "today_count": today_count,
+        "total_count": total_count
+    }
 
 name_cache = {}
 
@@ -31,8 +48,8 @@ async def get_name(app, id):
         return name_cache[id]
     else:
         try:
-            i = await app.get_users(id)
-            name = f'{(i.first_name or "")} {(i.last_name or "")}'
+            user_info = await app.get_users(id)
+            name = f'{(user_info.first_name or "")} {(user_info.last_name or "")}'
             name_cache[id] = name
             return name
         except Exception as e:
